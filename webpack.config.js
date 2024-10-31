ff08e7c43fcfe60ee95d0f1b8db573cbab64b6c2
Denis <denis.shagim@icloud.com>
2024-10-31 20:29:20 +0500

    init: create project

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+};
